1ffe85d12bfb45ff58d194c8fba7b4c4d3df9c2f
EFEX <nicolasm@hotmail.es>
2025-03-20 20:52:50 -0300

    BREAKING CHANGE: use only css
    Using CSS :target pseudo-class for the same approach

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,13 @@
-import OneClickView from './oneClickView.min.js';
-
-// OneClickView.setConfig({ containerClassName: 'oCv-container', pathImage: 'images/', zoom: false, patterns:{ thumbnail: '(_thumbnail)?', caseInsensitive: true } });
-OneClickView.initialize();
+(function(){
+  window.addEventListener('hashchange', function() {
+    var targetId = location.hash.substring(1);
+    var overlay = document.getElementById(targetId);
+    if (overlay) {
+      overlay.querySelectorAll('img[data-src]').forEach(function(img) {
+        if (!img.getAttribute('src')) {
+          img.src = img.getAttribute('data-src');
+        }
+      });
+    }
+  });
+})();
